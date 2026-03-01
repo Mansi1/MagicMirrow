5e30ffc49bbd4e4542ecf3a441a53fae69bee4d5
Michael Mannseicher <michael.mannseicher@doctarigroup.com>
2026-03-01 22:53:50 +0100

    refactor: go from chromium to surf

--- start_kiosk.sh
+++ start_kiosk.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
-
-# 1. Prevent the screen from turning off or going to sleep
+# Disable screen blanking
 xset s noblank
 xset s off
 xset -dpms
 
-# 2. Start a tiny window manager (prevents Chromium from "floating" awkwardly)
+# Start the window manager
 matchbox-window-manager -use_titlebar no &
 
-# 3. Launch Chromium in Kiosk Mode
-# Extra flags added to save RAM and CPU on the Pi Zero 2 W
-chromium \
-  --noerrdialogs \
-  --disable-infobars \
-  --kiosk \
-  --disable-gpu \
-  --check-for-update-interval=31536000 \
-  --num-raster-threads=2 \
-  http://localhost:8080
+# Start Surf (The Lightweight King)
+# -K = Kiosk mode, -F = Fullscreen
+surf -K -F http://localhost:8080
