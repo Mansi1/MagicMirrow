170a5275b28657e3eb22f98b73ffb9fc21fadb36
Michael Mannseicher <michael.mannseicher@doctarigroup.com>
2026-03-01 23:09:53 +0100

    refactor: use firefox now

--- start_kiosk.sh
+++ start_kiosk.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-# Disable screen blanking
+
+# 1. Wait 15 seconds for the MagicMirror server to fully breathe
+sleep 15
+
+# 2. Disable screen blanking/sleeping
 xset s noblank
 xset s off
 xset -dpms
 
-# Start the window manager
+# 3. Start the Window Manager (Required for Firefox to go Fullscreen)
 matchbox-window-manager -use_titlebar no &
 
-# Start Surf (The Lightweight King)
-# -K = Kiosk mode, -F = Fullscreen
-surf -K -F http://localhost:8080
+# 4. Start Firefox in Kiosk Mode
+# --no-remote prevents "already running" errors
+# --kiosk forces it to fill the screen
+firefox-esr --kiosk --no-remote http://localhost:8080
